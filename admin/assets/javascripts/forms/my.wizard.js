f1837b26c2a1f1a65dc67b3db180c9c2a345e8ba
muhjuanda <muhjuandab@gmail.com>
2017-07-07 15:54:17 +0800

    bismillah

---
+++ admin/assets/javascripts/forms/my.wizard.js
@@ -0,0 +1,70 @@
+(function($) {
+
+	'use strict';
+	
+	var $w4finish = $('#myw4').find('ul.pager li.finish'),
+		$w4validator = $("#myw4 form").validate({
+		highlight: function(element) {
+			$(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+		},
+		success: function(element) {
+			$(element).closest('.form-group').removeClass('has-error');
+			$(element).remove();
+		},
+		errorPlacement: function( error, element ) {
+			element.parent().append( error );
+		}
+	});
+
+	$w4finish.on('click', function( ev ) {
+		ev.preventDefault();
+		var validated = $('#myw4 form').valid();
+		if ( validated ) {
+/*			new PNotify({
+				title: 'Congratulations',
+				text: 'You completed the wizard form.',
+				type: 'custom',
+				addclass: 'notification-success',
+				icon: 'fa fa-check'
+			});*/
+			$('#myw4 form').submit();
+		}
+	});
+
+	$('#myw4').bootstrapWizard({
+		tabClass: 'wizard-steps',
+		nextSelector: 'ul.pager li.next',
+		previousSelector: 'ul.pager li.previous',
+		firstSelector: null,
+		lastSelector: null,
+		onNext: function( tab, navigation, index, newindex ) {
+			var validated = $('#myw4 form').valid();
+			if( !validated ) {
+				$w4validator.focusInvalid();
+				return false;
+			}
+		},
+		onTabClick: function( tab, navigation, index, newindex ) {
+			if ( newindex == index + 1 ) {
+				return this.onNext( tab, navigation, index, newindex);
+			} else if ( newindex > index + 1 ) {
+				return false;
+			} else {
+				return true;
+			}
+		},
+		onTabChange: function( tab, navigation, index, newindex ) {
+			var $total = navigation.find('li').size() - 1;
+			$w4finish[ newindex != $total ? 'addClass' : 'removeClass' ]( 'hidden' );
+			$('#myw4').find(this.nextSelector)[ newindex == $total ? 'addClass' : 'removeClass' ]( 'hidden' );
+		},
+		onTabShow: function( tab, navigation, index ) {
+			var $total = navigation.find('li').length - 1;
+			var $current = index;
+			var $percent = Math.floor(( $current / $total ) * 100);
+			$('#myw4').find('.progress-indicator').css({ 'width': $percent + '%' });
+			tab.prevAll().addClass('completed');
+			tab.nextAll().removeClass('completed');
+		}
+	});
+}).apply(this, [jQuery]);;
---
+++ js/custom.js
@@ -0,0 +1,5 @@
+function menu() {
+	$('.ui.sidebar')
+  .sidebar('toggle')
+;
+} 
